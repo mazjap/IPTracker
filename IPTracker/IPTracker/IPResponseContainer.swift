7c72ed0f381600ddb9744442650bf3b398b186e3
Jordan Christensen <jordan.c4922@gmail.com>
2020-10-19 13:33:08 -0600

    Add background image, create IP model and make it conform to the decodable protocol

---
+++ IPTracker/IPTracker/IPResponseContainer.swift
@@ -0,0 +1,66 @@
+//
+//  IPResponseContainer.swift
+//  IPTracker
+//
+//  Created by Jordan Christensen on 10/19/20.
+//
+
+import Foundation
+
+struct IPResponseContainer: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case ip
+        case location
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        ip = try IP(container.decode(String.self, forKey: .ip))
+        location = try container.decode(Location.self, forKey: .location)
+    }
+    
+    let ip: IP
+    let location: Location
+}
+
+struct IP: CustomStringConvertible {
+    enum IPErrors: Error {
+        case incompleteValues
+    }
+    
+    var description: String {
+        "\(val1).\(val2).\(val3).\(val4)"
+    }
+    
+    init(_ ip: String) throws {
+        let intArr = ip.split(separator: ".").compactMap { UInt8($0) }
+        guard intArr.count == 4 else { throw IPErrors.incompleteValues }
+        
+        self.val1 = intArr[0]
+        self.val2 = intArr[1]
+        self.val3 = intArr[2]
+        self.val4 = intArr[3]
+    }
+    
+    let val1: UInt8
+    let val2: UInt8
+    let val3: UInt8
+    let val4: UInt8
+}
+
+struct Location: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case country
+        case city
+        case latitude = "lat"
+        case longitude = "lng"
+        case postalCode
+    }
+    
+    var country: String
+    var city: String
+    var latitude: Double
+    var longitude: Double
+    var postalCode: String
+}
--- IPTracker/IPTracker/ViewController.swift
+++ IPTracker/IPTracker/ViewController.swift
@@ -1,19 +1,27 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  IPTracker
 //
 //  Created by Jordan Christensen on 10/19/20.
 //
 
-import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class MapViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination
+        
+        if segue.identifier == "", let destVC = destination as? MapViewController {
+            print(destVC)
+        }
+    }
 }
 
